10590af618e8e368b7966f74a24b9462f957226c
Danila <dbulbakov@bk.ru>
2024-04-10 11:15:29 +0600

    I added controler which i deleted ocasionally

--- src/main/java/com/example/springboot_rest/controller/MyController.java
+++ src/main/java/com/example/springboot_rest/controller/MyController.java
@@ -1,57 +1,8 @@
 package com.example.springboot_rest.controller;
-import com.example.springboot_rest.entity.Employee;
-import com.example.springboot_rest.service.EmployeeService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 
-@RestController
-@RequestMapping("/api")
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class MyController {
-
-    @Autowired
-    private EmployeeService employeeService;
-
-    @RequestMapping( value = "/employees")
-    public List<Employee> showAllEmployees() {
-        List<Employee> employees = employeeService.getAllEmployees();
-        return employees;
-    }
-
-    @GetMapping("/employees/{id}")
-    public Employee getEmployee(@PathVariable int id) {
-        Employee employee = employeeService.getEmployee(id);
-        return employee;
-    }
-
-    @PostMapping("/employees")
-    public Employee addNewEmployee(@RequestBody  Employee employee) {
-        employeeService.saveEmployee(employee);
-        return employee;
-    }
-
-    @PutMapping("/employees")
-    @Transactional
-    public Employee updateEmployee(@RequestBody Employee employee) {
-        employeeService.saveEmployee(employee);
-        return employee;
-    }
-
-    @DeleteMapping("/employees/{id}")
-    @Transactional
-    public String deleteEmployee(@PathVariable int id) {
-        employeeService.deleteEmployee(id);
-        return "Employee with ID = " +id + " was deleted";
-
-    }
-
-    @GetMapping("/employees/name/{name}")
-    public List<Employee> showEmployee(@PathVariable String name) {
-        List<Employee> employees = employeeService.findAllByName(name);
-        return employees;
-    }
-
-
 }
